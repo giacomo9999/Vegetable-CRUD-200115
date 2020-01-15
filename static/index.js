1f3b80067ec675017db2d3613ca6d85ccff4862c
James Gary <gmarchesi888@gmail.com>
2020-01-15 18:34:57 -0500

    beginning to write DOM manipulation.

--- static/index.js
+++ static/index.js
@@ -3,7 +3,17 @@ console.log("JS In The House");
 axios
   .get("http://localhost:3000/api/vegetables")
   .then(response => {
-    console.log(response.data.data);
+    const oneVeg = response.data.data[0].name;
+    const twoVeg = response.data.data[1].name;
+    console.log(oneVeg, twoVeg);
+    const vegList = document.createElement("ul");
+    document.querySelector(".container-outer").appendChild(vegList);
+    const vegetable = document.createElement("li");
+    const vegetable2 = document.createElement("li");
+    vegetable.innerHTML = oneVeg;
+    vegetable2.innerHTML = twoVeg;
+    vegList.appendChild(vegetable);
+    vegList.appendChild(vegetable2);
   })
   .catch(function(error) {
     console.log(error);
